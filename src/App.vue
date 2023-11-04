7a121c30de4b575c7577245cb5b2316f3b333129
Flavio Campos <flaviorcampos15@gmail.com>
2023-11-04 12:16:48 -0300

    Até aula 29

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <template>
+  <div>
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
+      <template v-for="answer in this.answers" :key="answer">
+        <input type="radio" name="options" value="answer" />
+        <label v-html="answer"> </label><br />
+      </template>
+    </template>
+
+    <button class="send" type="button">Send</button>
+  </div>
 </template>
 
+//https://opentdb.com/api.php?amount=1&category=18
+//https://opentdb.com/api.php?amount=10&category=21
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const api = "https://opentdb.com/api.php?amount=1&category=21";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      question: undefined,
+      incorrect_answers: undefined,
+      correct_answer: undefined,
+    };
+  },
+  computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrect_answers));
+      answers.splice(
+        Math.round(Math.random() * answers.length),
+        0,
+        this.correct_answer
+      );
+      return answers;
+    },
+  },
+  created() {
+    this.axios.get(api).then((response) => {
+      const registro = response.data.results[0];
+
+      this.question = registro.question;
+      this.incorrect_answers = registro.incorrect_answers;
+      this.correct_answer = registro.correct_answer;
+    });
+  },
+};
 </script>
 
 <style lang="scss">
@@ -21,6 +56,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type="radio"] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
